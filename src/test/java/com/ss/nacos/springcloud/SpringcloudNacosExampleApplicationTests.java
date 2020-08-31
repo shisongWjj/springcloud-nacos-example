2a18863d92ce901a012f80ba53b49578cd5b611d
song.shi <song.shi@atzuche.com>
2020-08-31 18:43:54 +0800

    first commit

---
+++ src/test/java/com/ss/nacos/springcloud/SpringcloudNacosExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.nacos.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudNacosExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
